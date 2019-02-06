137c547bdd101040a33fb27cd44d6a5313fc0a4c
Moisés Dandico <moises.dandico23@gmail.com>
2019-02-06 12:03:13 -0200

    Upload inicial de códigos

--- pericia/conectadb.php
+++ pericia/conectadb.php
@@ -1,9 +1,12 @@
 <?php
 function db_connect()
 {
-    $pdo = new PDO("mysql:host=localhost;dbname=pericia_homolog","root","wolfpack");
-  
-    return $pdo;
+    $pdo = new PDO("mysql:host=localhost;dbname=pericia_homolog","root","wolfpack",array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    )
+);
+
+return $pdo;
 }
 
 ?>
